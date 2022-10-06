fee6b6f360945ded9a584b75420baeceb045d299
Jai7059 <jkhedad@gmail.com>
2022-10-06 00:24:52 -0700

    commit neela

---
+++ file.js
@@ -0,0 +1,16 @@
+var total=0;
+var count=0;
+while (count<5)
+{
+    var marks;
+    total=total+marks;
+    count=count+1;
+}
+function myfunc()
+{
+    let p=prompt("enter")
+    if(p!=null)
+    {
+        document.getElementById('demo').innerHTML="hello"+p+"how are you"
+    }
+}
